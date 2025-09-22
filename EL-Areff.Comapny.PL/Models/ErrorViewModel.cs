408243626ffd73429a7219f47ddd667718223ff8
Ahmed Emad <ahmedbanhawy538@gamil.com>
2025-09-23 00:23:16 +0300

    Add project files.

---
+++ EL-Areff.Comapny.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EL_Areff.Comapny.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

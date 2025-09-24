d8ecd8c7140ad6b4c6804fb7113f57e8709e893b
Ahmed Emad <ahmedbanhawy538@gamil.com>
2025-09-24 20:47:36 +0300

    create create  (Action-view)

--- EL-Areff.Comapny.PL/Controllers/DepartmentController.cs
+++ EL-Areff.Comapny.PL/Controllers/DepartmentController.cs
@@ -2,6 +2,7 @@
 using EL_Areff.Comapny.BLL.Iterfaces;
 using EL_Areff.Comapny.BLL.Repositories;
 using EL_Areff.Comapny.DAL.Model;
+using EL_Areff.Comapny.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EL_Areff.Comapny.PL.Controllers
@@ -20,5 +21,34 @@ namespace EL_Areff.Comapny.PL.Controllers
             var departments = _departmentRepository.GetAll();
             return View(departments);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if(ModelState.IsValid)
+            {
+                var department = new Department()
+                {
+                    Code = model.Code,
+                    Name = model.Name,
+                    CraeteAt= model.CraeteAt,
+                };
+                var result = _departmentRepository.Add(department);
+                if(result>0)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View(model);
+        }
+
+      
     }
 }

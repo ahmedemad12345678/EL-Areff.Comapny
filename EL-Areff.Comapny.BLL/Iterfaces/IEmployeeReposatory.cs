7ce2a27065950757ddff39f65347fd988b9f30b2
Ahmed Emad <ahmedbanhawy538@gamil.com>
2025-09-26 17:13:41 +0300

    Implementation  Of BLL For Employee Service Before Genaric

---
+++ EL-Areff.Comapny.BLL/Iterfaces/IEmployeeReposatory.cs
@@ -0,0 +1,18 @@
+﻿using EL_Areff.Comapny.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EL_Areff.Comapny.BLL.Iterfaces
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee? Get(int id);
+        int Add(Employee model);
+        int Update(Employee model);
+        int Delete(Employee model);
+    }
+}
